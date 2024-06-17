038bdefd29941fbc3abff9268d67c0f26babe562
Yesset Bimanov <eset.bimanov@gmail.com>
2024-06-17 18:47:28 +0500

    New commit message

--- src/components/Dashboard/CreateDiels.vue
+++ src/components/Dashboard/CreateDiels.vue
@@ -4,6 +4,8 @@ import {DB_ID, COLLECTION_DEALS} from '../../appwrite.const'
 import {DB} from '../../utils/appwrite'
 import { v4 as uuidv4 } from 'uuid';
 import { account } from '../../utils/appwrite';
+import { ref } from 'vue';
+import { useForm } from 'vee-validate';
 
 const onFormSubmit = ref<bolian>(false)
 
@@ -14,6 +16,40 @@ const onFormSubmit = ref<bolian>(false)
    }
    status: string
  }
+
+ const props = defineProps({
+   status {
+     type: string
+     Default: ''
+   },
+   refetch: {
+     type: Function
+   }
+
+ })
+
+ const { handleSubmit, defineField, resetForm } = useForm<IDealFormState>(
+  initialValues: {
+    status: props.status
+  }
+ );
+
+ const [name, nameAtter] = defineField('name')
+ const [eamil, emailAtter] = defineField('name')
+ const [customerEmail, customerEmailAtter] = defineField('customer.email')
+ const [customerName, customerNameAtte] = defineField('customer.name')
+
+
+ const { mutate} = useMutation(async () => {
+   mutateKey:['create a new deal']
+   mutateFn: {data} => {
+     return DB.createDocument(DB_ID, COLLECTION_DEALS, uuidv4(), data)
+   },
+   onSuccess: () => {
+     props.refetch = props.refetch()
+     
+   }
+ })
 
  
 </script>
